a143a9158c84444fd0b499f00f14d47b5cb71dd9
Diaaana <dianaradomskaya19@gmail.com>
2018-09-03 10:24:28 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '691180471:AAF5oEGZG-uvkns-sUOLuraWp2GnboRNkjM'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'Hello: "Hi, ${msg.from.first_name}"')
+})
+
